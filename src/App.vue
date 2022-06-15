db8d5121dcf3cb9eedf497a015bff9b5c24f7503
Rahul Paul <rahul.cse.ru@gmail.com>
2022-06-15 20:53:32 +0600

    first at blog commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import addBlog from "./components/addBlog.vue"
+import showBlog from "./components/showBlog.vue"
+import listBlogs from "./components/listBlogs.vue"
+import header from "./components/header.vue"
+
+export default {
+  components:{
+    "add-blog": addBlog,
+    "show-blog": showBlog,
+    "list-blog" :listBlogs,
+    "app-header": header
+  },
+  data () {
+    return {
+      
+    }
+  }, 
+  methods:{
+
+  }
+}
+</script>
+
+<style>
+body{
+  margin: 0;
+  font-family: "Nunito Semibold";
+}
+</style>
